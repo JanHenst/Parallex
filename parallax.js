a0b9fb12b39620333c024e595a3c80a391e51337
JanHenst <42889823+JanHenst@users.noreply.github.com>
2019-10-15 11:52:30 +0200

    Add files via upload

--- parallax.js
+++ parallax.js
@@ -2,6 +2,9 @@ let scrollAfstand;
 let sectie2 = document.getElementsByClassName('sectie--2')[0];
 let sectie4 = document.getElementsByClassName('sectie--4')[0];
 let sectie6 = document.getElementsByClassName('sectie--6')[0];
+let indicator = document.getElementById('scrollIndicator');
+const documentHoogte = document.body.clientHeight;
+const viewportHoogte = window.innerHeight;
 
 // als er 902px is gescrolled moet de parallax in sectie--4 uit elkaar
 
@@ -13,7 +16,10 @@ window.addEventListener('scroll', (e) => {
    corrSection6(scrollAfstand);
    if (scrollAfstand >= 902) {
      verwijderClassKop();
+   } else {
+     voegClassKopToe();
    }
+   werkIndicatorBij(scrollAfstand);
 });
 
 const corrSection2 = (gescrolled) => {
@@ -31,4 +37,11 @@ const corrSection6 = (gescrolled) => {
 
 const verwijderClassKop = () => {
   document.querySelector('.sectie--4 h2').classList.remove('sectie--4__kop');
+}
+const voegClassKopToe = () => {
+  document.querySelector('.sectie--4 h2').classList.add('sectie--4__kop');
+}
+
+const werkIndicatorBij = (getal) => {
+  indicator.style.width = 100*getal/(documentHoogte-viewportHoogte) +'%';
 }
